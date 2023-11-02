0ad16e14299c6783bbca0aa01ccf6b09ea5adafe
ellarohling <142920666+ellarohling@users.noreply.github.com>
2023-11-01 22:18:49 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,69 @@
+$(function () {
+    console.log("ready!");
+  
+    $("#loadProfile").on("click", function (e) {
+  
+      e.preventDefault(); 
+      console.log("button clicked: ");
+  
+      $.ajax({
+        url: "https://ellarohling.github.io/solid-potato-india/assets/data.json",
+        dataType: "json",
+        success: function (data) {
+          // console.log(data.uname);
+          // $("#uname").val(data.uname);
+          // console.log(data.optIn);
+  
+          $("#optIn").prop("checked", data.optIn);
+          $("#bcool").prop("checked", data.cool);
+  
+  
+          $('input[type=radio]').each(function () {
+            if (data.spaghet === $(this).val())
+            {
+              $(this).prop('checked', true)
+                console.log($(this));
+           }
+  
+        });
+  
+          $.each(data, function (key, val) {
+            console.log(key, val);
+            $(`#${key}`).val(val);
+          });
+  
+        },
+        
+      });
+
+  });
+  
+  //date and time JS
+  var dt = new Date();
+  document.getElementById('date-time').innerHTML=dt;
+  
+  function myFunction() {
+      var x = document.getElementById("myInput");
+      if (x.type === "password") {
+        x.type = "text";
+      } else {
+        x.type = "password";
+      }
+    }
+   
+    //click the eye icon
+      $(".reveal").on('click',function() {
+        let $pwd = $(".pwd");
+        
+        if ($pwd.attr('type') === 'password') {
+            $pwd.attr('type', 'text');
+          $(this).children('i').addClass('bi-eye-slash-fill').removeClass('bi-eye-fill');
+        } else {
+            $pwd.attr('type', 'password');
+        $(this).children('i').removeClass('bi-eye-slash-fill').addClass('bi-eye-fill');
+        }
+    });
+      
+  
+  
+  });
